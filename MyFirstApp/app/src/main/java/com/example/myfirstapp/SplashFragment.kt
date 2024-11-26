396974dfd7b5f28e22f5d3dd19acd31bc5d7a778
SmirnovPavel8 <PASmirnov@stud.kantiana.ru>
2024-11-26 11:43:56 +0200

    add

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/SplashFragment.kt
@@ -0,0 +1,46 @@
+package com.example.myfirstapp
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SplashFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SplashFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root =inflater.inflate(R.layout.fragment_splash,container,false)
+        val navController=NavHostFragment.findNavController(this)
+        val registrationData=requireActivity().getSharedPreferences("registrationData", Context.MODE_PRIVATE)
+        val automaticEnter=requireActivity().getSharedPreferences("automaticLogin",Context.MODE_PRIVATE)
+       if(registrationData.contains("login")) {
+           if (automaticEnter.getBoolean("automaticEnterFlag", false)) {
+               navController.navigate(R.id.oneFragment)
+           } else {
+               navController.navigate(R.id.loginFragment)
+           }
+       }
+       else{
+            navController.navigate(R.id.registrationFragment)
+        }
+        return root
+    }
+
+
+}
