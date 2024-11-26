7f3bbd7e3f6443b65f9f4bba3c7fae95a95aa3bc
SmirnovPavel8 <93121464+SmirnovPavel8@users.noreply.github.com>
2024-11-26 19:48:55 +0200

    Add files via upload

---
+++ src/main/java/com/example/myfirstapp/Fragment2.kt
@@ -0,0 +1,30 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Fragment2.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Fragment2 : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_2, container, false)
+    }
+
+
+}
